df4a8df1a0b6b06154fce5990d11821924a42274
kwiecs01 <szymon.kwiecinski@stepstone.com>
2023-03-11 19:32:30 +0100

    boilerplate2

--- app.py
+++ app.py
@@ -2,5 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def index():
+    return render_template('index.html', title="Habit tracker - Home")
+
+
+@app.route('/add', methods=["GET", "POST"])
+def add_habit():
+    return render_template("add_habit.html", title="Habit Tracker - Add Habit")
+
+
 if __name__ == '__main__':
     app.run()
